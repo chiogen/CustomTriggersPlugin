6cfbf11e9ae073047a146e840a7741e4306e90da
chiogen <iwashier@gmail.com>
2025-06-29 11:45:08 +0200

    More cleanup

--- CustomTriggers/Trigger.cs
+++ CustomTriggers/Trigger.cs
@@ -1,24 +1,12 @@
 using CustomTriggersPlugin.Enums;
-using System.Collections.Generic;
 
 namespace CustomTriggersPlugin;
 
 public class Trigger
 {
-
-    public static List<Trigger> GetDebugTriggers()
-    {
-        List<Trigger> triggers = [];
-
-
-        return triggers;
-    }
-
     public string Key { get; set; } = "custom";
     public string Name { get; set; } = "";
     public ChatType? ChatType { get; set; }
     public string Pattern { get; set; } = "";
-
     public string SoundData { get; set; } = "";
-
 }
